04b141dd6b8f55614cc131d68795ba890238fec1
Tatsiana Stsepchanka <tstsepchanka@slb.com>
2021-04-03 13:50:47 +0300

    fix: password validation

--- src/models/user.ts
+++ src/models/user.ts
@@ -11,7 +11,7 @@ export type User = {
 export const userSchema = Joi.object().keys({
   id: Joi.string().uuid().optional(),
   login: Joi.string().required(),
-  password: Joi.string().required(),
+  password: Joi.string().alphanum().required(),
   age: Joi.number().min(4).max(130).required(),
   isDeleted: Joi.boolean().optional(),
 });

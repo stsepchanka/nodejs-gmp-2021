6c5558f8e403b460a311e6f307b065955d98973b
Tatsiana Stsepchanka <tstsepchanka@slb.com>
2021-05-15 19:36:58 +0300

    improve: logging

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,6 +1,7 @@
 import { createLogger, format, transports } from "winston";
 
 export const logger = createLogger({
+  format: format.combine(format.timestamp(), format.json()),
   transports: [
     new transports.File({
       filename: "error.log",
--- src/middleware/catch-errors.ts
+++ src/middleware/catch-errors.ts
@@ -14,38 +14,58 @@ export function catchErrors(
   res: Response,
   next: NextFunction
 ): Response {
-  const { url, method } = req;
+  const { url, body, query, method } = req;
 
   if (err instanceof ValidationError) {
     logger.error(
-      `${method} ${url}; validation error: ${JSON.stringify(
-        err.details.errors
-      )}`
+      `${method} ${url}; body: ${JSON.stringify(
+        body
+      )}; query params: ${JSON.stringify(
+        query
+      )}; validation error: ${JSON.stringify(err.details.errors)}`
     );
     return res.status(err.status).json(err.details);
   }
 
   if (err instanceof NotFoundError) {
-    logger.error(`${method} ${url}; resource not found: ${err.text}`);
+    logger.error(
+      `${method} ${url}; body: ${JSON.stringify(
+        body
+      )}; query params: ${JSON.stringify(query)}; resource not found: ${
+        err.text
+      }`
+    );
     return res.sendStatus(err.status);
   }
 
   if (err instanceof IncorrectRequestToDBError) {
-    logger.error(`${method} ${url}; incorrect request to DB: ${err.text}`);
+    logger.error(
+      `${method} ${url}; body: ${JSON.stringify(
+        body
+      )}; query params: ${JSON.stringify(query)}; incorrect request to DB: ${
+        err.text
+      }`
+    );
     return res.sendStatus(err.status);
   }
 
   if (err instanceof SequelizeValidationError) {
     logger.error(
-      `${method} ${url}; sequelize validation error: ${JSON.stringify(
-        err.errors
-      )}`
+      `${method} ${url}; body: ${JSON.stringify(
+        body
+      )}; query params: ${JSON.stringify(
+        query
+      )}; sequelize validation error: ${JSON.stringify(err.errors)}`
     );
     return res.sendStatus(StatusCodes.BAD_REQUEST);
   }
 
   if (err instanceof Error) {
-    logger.error(`${method} ${url}; unhandled error: ${err}`);
+    logger.error(
+      `${method} ${url}; body: ${JSON.stringify(
+        body
+      )}; query params: ${JSON.stringify(query)}; unhandled error: ${err}`
+    );
     return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
   }
 
